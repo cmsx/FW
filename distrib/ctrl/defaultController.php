81b49d48f8e3cead2f02b13715a6ca1db280774f
Igor Osharin <igor@osharin.ru>
2013-05-15 18:59:38 +0300

    Ядро системы переписано на symfony/http-kernel

--- distrib/ctrl/defaultController.php
+++ distrib/ctrl/defaultController.php
@@ -2,6 +2,7 @@
 
 use CMSx\Page;
 use CMSx\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class defaultController extends Controller
 {
@@ -12,5 +13,31 @@ class defaultController extends Controller
     $p->setText('<p>It works :)</p>');
 
     return $p;
+  }
+
+  /** Экшн для отображения возникающих ошибок. Отображается через подзапрос */
+  function errorAction()
+  {
+    /** @var $e Symfony\Component\Debug\Exception\FlattenException */
+    if (!$e = $this->getRequest()->attributes->get('exception')) {
+      $this->notFound('Exception отсутствует в запросе');
+    }
+
+    $code = $e->getStatusCode();
+    if (404 == $code) {
+      $title = 'Страница не найдена!';
+    } elseif (403 == $code) {
+      $title = 'Доступ запрещен!';
+    } else {
+      $title = 'Ошибка!';
+    }
+
+    $p = new Page();
+    $p->setTitle($title);
+    if (DEVMODE) {
+      $p->setText($e->getMessage());
+    }
+
+    return Response::create($p, $code, $e->getHeaders());
   }
 }
--- distrib/web/index.php
+++ distrib/web/index.php
@@ -2,8 +2,33 @@
 
 require_once __DIR__ . '/../inc/init.php';
 
-use CMSx\Router;
+use CMSx\EventListener;
+use CMSx\ControllerResolver;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
-$r = new Router(null, DIR_CTRL);
-$r->enableDebug(DEVMODE);
-$r->process();
+// Создаем объект Request
+$request = Request::createFromGlobals();
+
+// Диспетчер событий
+$dispatcher = new EventDispatcher();
+
+// Добавляем слушателя событий системы
+$dispatcher->addSubscriber(new EventListener());
+
+// Класс, находящий контроллер по URL
+$resolver = new ControllerResolver(DIR_CTRL);
+
+// Ядро преобразует запрос в ответ
+$kernel = new HttpKernel($dispatcher, $resolver);
+
+// Обрабатываем запрос
+$response = $kernel->handle($request);
+
+// Возвращаем результат
+$response->send();
+
+// Выполняем отложенные задачи, если они есть
+$kernel->terminate($request, $response);
